3df1f6cde9db14824f71ab186075c95d5ecb603c
Valentyn Demchuk <vdemchuk.vd@gmail.com>
2018-07-23 23:07:22 +0300

    first commit

---
+++ Wheels/Models/Make.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wheels.Models
+{
+    public class Make
+    {
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public ICollection<Model> Models { get; set; }
+
+		public Make()
+		{
+			Models = new Collection<Model>();
+		}
+    }
+}
