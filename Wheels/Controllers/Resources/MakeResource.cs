9723929bc809fb1e0d4259099e54008ee4faefb1
Valentyn Demchuk <vdemchuk.vd@gmail.com>
2018-07-25 00:10:01 +0300

    Add Resources

---
+++ Wheels/Controllers/Resources/MakeResource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Wheels.Models;
+
+namespace Wheels.Controllers.Resources
+{
+    public class MakeResource
+    {
+			public int Id { get; set; }
+			public string Name { get; set; }
+
+			public ICollection<Model> Models { get; set; }
+
+			public MakeResource()
+			{
+				Models = new Collection<Model>();
+			}
+	}
+}
---
+++ Wheels/Controllers/Resources/ModelResource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wheels.Controllers.Resources
+{
+    public class ModelResource
+    {
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public ICollection<ModelResource> Models { get; set; }
+
+		public ModelResource()
+		{
+			Models = new Collection<ModelResource>();
+		}
+	}
+}
