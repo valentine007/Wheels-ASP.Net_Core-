33d19c6c0b28c1e68075470d88127252012ecf1e
Valentyn Demchuk <vdemchuk.vd@gmail.com>
2018-07-25 23:10:36 +0300

    using AutoMapper

--- Wheels/Controllers/MakesController.cs
+++ Wheels/Controllers/MakesController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Wheels.Controllers.Resources;
 using Wheels.Models;
 using Wheels.Persistence;
 
@@ -12,16 +14,19 @@ namespace Wheels.Controllers
     public class MakesController : Controller
     {
 		private readonly WheelsDBContext context;
+		private readonly IMapper mapper;
 
-		public MakesController(WheelsDBContext context)
+		public MakesController(WheelsDBContext context, IMapper mapper)
 		{
 			this.context = context;
+			this.mapper = mapper;
 		}
 
 		[HttpGet("/api/makes")]
-		public async Task<IEnumerable<Make>> GetMakes()
+		public async Task<IEnumerable<MakeResource>> GetMakes()
 		{
-			return await context.Makes.Include(m => m.Models).ToListAsync();
+			var makes = await context.Makes.Include(m => m.Models).ToListAsync();
+			return mapper.Map<List<Make>, List<MakeResource>>(makes);
 		}
     }
 }
--- Wheels/Startup.cs
+++ Wheels/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -23,6 +24,8 @@ namespace Wheels
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddAutoMapper();
+
 			services.AddDbContext<WheelsDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
 
             services.AddMvc();
